3e3102cd5c847974d6cc269d7d2d470a951a358c
Oleg Nedbaylo <olned64@gmail.com>
2020-06-16 18:42:32 +0200

    license header addeed

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,8 @@
+# Copyright Oleg Nedbaylo 2020.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE
+# or copy at http://www.boost.org/LICENSE_1_0.txt
+
 from unittest import TestCase, main
 import ssc2ce_cpp as m
 
